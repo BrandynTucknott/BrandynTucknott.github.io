b5183cb7300c471d43ce9ffbaa3e5b215bd33f3a
BrandynTucknott <114444356+BrandynTucknott@users.noreply.github.com>
2023-01-19 14:29:15 -0800

    push

--- world_peace_ai/global_variables.js
+++ world_peace_ai/global_variables.js
@@ -1,1 +1,4 @@
-let currentStage = 0;
+let currentStage = 0; // current stage of the game the player is on
+let sectionIStages = []; // list of all section I stages in the game
+let sectionIIStages = []; // list of all section II stages in the game
+let sectionIIIStages = []; // list of all section III stages in the game
---
+++ world_peace_ai/scriptAdder.js
@@ -0,0 +1,5 @@
+// add all js files in section I
+
+// add all js files in section II
+
+// add all js files in section III
