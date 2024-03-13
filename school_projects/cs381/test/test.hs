b11988b3b1713e8690dad5540d97246a746dc0b4
BrandynTucknott <114444356+BrandynTucknott@users.noreply.github.com>
2024-03-13 12:59:06 -0700

    cs 381 hw6 complete

--- school_projects/cs381/test/test.hs
+++ school_projects/cs381/test/test.hs
@@ -1,28 +1,6 @@
-h _ [] = 0
-h y (x:xs) 
-    | y > 0 = y + h (y) xs
-    | otherwise = 0
-
-g _ [] = []
-g y (x:xs) 
-    | y>0       = g (y-1) xs
-    | otherwise = xs
-
--- func []
-func_tail :: [[Integer]] -> [[Integer]]
-func_tail list = map tail list
-
--- func_head :: [[Integer]] -> [[Integer]]
--- func_head list = map head list
-
-func_rev :: [[Integer]] -> [[Integer]]
-func_rev list = map reverse list
-
 main :: IO ()
-main = do
-    -- print (h 5 [1,2,3,4,5,6,7])
-    -- print(func_tail [[1,2,3], [3,4], [5,6]])
-    -- print(func_head [[1,2,3], [3,4], [5,6]])
-    print(func_rev [[1,2,3], [3,4], [5,6]])
-    print (func_rev [1,2,4])
-    -- print( g 1 [1,2,3,4,5,6,7,8,9,10] )
+main =
+    do
+        let x = 2 in
+            print(let x=1 in x,x)
+    
